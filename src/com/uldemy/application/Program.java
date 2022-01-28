7b00235810fc4ac2a526547d8b2b1f4342cc115f
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-01-28 03:05:03 -0300

    Starting to implement Board and Piece

--- src/com/uldemy/application/Program.java
+++ src/com/uldemy/application/Program.java
@@ -1,11 +1,10 @@
 package com.uldemy.application;
 
-import com.uldemy.boardgame.Position;
+import com.uldemy.boardgame.Board;
 
 public class Program {
     public static void main(String[] args) {
-        Position pos = new Position(3,2);
-
-        System.out.println(pos);
+        Board board = new Board(8,8);
+        
     }
 }
