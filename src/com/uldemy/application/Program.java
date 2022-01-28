7f4b1788754e24730d5be255ba05b9c011557a2d
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-01-28 02:56:22 -0300

    First class: Position

--- src/com/uldemy/application/Program.java
+++ src/com/uldemy/application/Program.java
@@ -1,7 +1,11 @@
 package com.uldemy.application;
 
+import com.uldemy.boardgame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Position pos = new Position(3,2);
+
+        System.out.println(pos);
     }
 }
---
+++ src/com/uldemy/boardgame/Position.java
@@ -0,0 +1,28 @@
+package com.uldemy.boardgame;
+
+public class Position {
+    private  int row, column;
+
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setValues(int row, int column){
+        this.row = row;
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+}
