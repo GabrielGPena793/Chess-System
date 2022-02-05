f291592bb77da78e058be787180a0f4774e36735
GabrielGPena793 <kbeca.gabriel793@gmail.com>
2022-02-05 15:21:54 -0300

    Pawn

---
+++ src/com/uldemy/chess/pieces/Pawn.java
@@ -0,0 +1,81 @@
+package com.uldemy.chess.pieces;
+
+import com.uldemy.boardgame.Board;
+import com.uldemy.boardgame.Position;
+import com.uldemy.chess.ChessPiece;
+import com.uldemy.chess.Color;
+
+public class Pawn extends ChessPiece {
+
+    public Pawn(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColmns()];
+
+        Position p = new Position(0,0);
+
+        if (getColor() == Color.WHITE){
+            //Brancas
+            //verifica uma casa a frente
+            p.setValues(position.getRow() - 1, position.getColumn());
+            if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+
+            //verifica uma casa a frente e a casa seguinte caso seja o primeiro movimento do peão
+            p.setValues(position.getRow() - 2, position.getColumn());
+            Position p2 = new Position(position.getRow() - 1, position.getColumn());
+            if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0){
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+
+            //verifica se tem um inimigo na diagonal para poder capturar
+            p.setValues(position.getRow() - 1, position.getColumn() - 1);
+            if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+
+            //verifica se tem um inimigo na diagonal para poder capturar
+            p.setValues(position.getRow() - 1, position.getColumn() + 1);
+            if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+        }
+        else {
+            //Pretas
+            //verifica uma casa a frente
+            p.setValues(position.getRow() + 1, position.getColumn());
+            if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)){
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+
+            //verifica uma casa a frente e a casa seguinte caso seja o primeiro movimento do peão
+            p.setValues(position.getRow() + 2, position.getColumn());
+            Position p2 = new Position(position.getRow() + 1, position.getColumn());
+            if (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p) && getBoard().positionExists(p2) && !getBoard().thereIsAPiece(p2) && getMoveCount() == 0){
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+
+            //verifica se tem um inimigo na diagonal para poder capturar
+            p.setValues(position.getRow() + 1, position.getColumn() - 1);
+            if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+
+            //verifica se tem um inimigo na diagonal para poder capturar
+            p.setValues(position.getRow() + 1, position.getColumn() + 1);
+            if (getBoard().positionExists(p) && isThereOpponentPiece(p)){
+                mat[p.getRow()][p.getColumn()] = true;
+            }
+        }
+        return mat;
+    }
+
+    @Override
+    public String toString() {
+        return "P";
+    }
+}
